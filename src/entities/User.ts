8a1e12f8d778a5b83205426337dc10cafd35f4b5
ThalesTrombim <thalestr@outlook.com>
2021-10-04 20:28:57 -0300

    todas as classes definidas

---
+++ src/entities/User.ts
@@ -0,0 +1,32 @@
+import {Column, CreateDateColumn, Entity, PrimaryColumn} from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @CreateDateColumn()
+    Updated_at: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
+}
+
+export { User }; 
